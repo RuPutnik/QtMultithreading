476bf79e24c83893664fa825fee2d7e1ccc1890a
putnik13 <ilrum1398@gmail.ru>
2023-03-18 20:46:02 +0300

    add message with main thread id

--- mainclass.cpp
+++ mainclass.cpp
@@ -4,13 +4,14 @@
 MainClass::MainClass(QObject *parent)
     : QObject{parent}
 {
-    taskThread=new QThread;
-    tsk=new Task;
+    taskThread=new QThread();
+    tsk=new Task();
 
     tsk->moveToThread(taskThread);
 }
 
 void MainClass::runTest()
 {
+    qDebug()<<"Main thread id: "<<QThread::currentThreadId();
 
 }
